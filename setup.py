71527b18586bafe667c761c70b4ab00358f166ea
kshitijkutumbe <kshitijkutumbe@gmail.com>
2024-08-25 20:34:28 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI-based-Source-Code-Analysis',
+    version= '0.0.0',
+    author= 'Kshitij Kutumbe',
+    author_email= 'kshitijkutumbe@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
